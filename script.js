38727df38235b96cfc5759aca5d4b469acdfdfc0
rapha <raphaelafelixdearaujo@gmail.com>
2026-03-10 09:20:46 -0300

    quarto commit

--- script.js
+++ script.js
@@ -1,146 +1,111 @@
-// CONSUMINDO UMA  API COM JAVASCRIPT
-// API: https://dog.ceo/api
+/*/*
+CONSUMINDO UMA API COM JAVASCRIPT
+API: https://dog.ceo/api
+*/
 
+//================================
+// 1- SELECIONAR ELEMENTOS DO HTML
+//================================
 
-// 1 - SELECIONAR OS ELEMENTOS DO HTML
-
-// Pegar a imagem do cachorro
 const dogImage = document.getElementById("dogImage");
-// elemento onde aparece o nome da raça
 const breedName = document.getElementById("breedName");
-
-// botão que busca cachorro aleatório
-const randomBtn = document.getElementById("randomBtn")
-
-// botão que busca a raça
-const searchBtn = document.getElementById("searchBtn")
-
-// campo de input onde o usuário digita a raça 
+const randomBtn = document.getElementById("randomBtn");
+const searchBtn = document.getElementById("searchBtn");
 const breedInput = document.getElementById("breedInput");
-
-// área onde a imagem aparece
 const dogArea = document.querySelector(".dog-area");
 
+//=======================
+// 2- URL BASE DA API
+//=======================
 
-// ==============================================
-// 2 - URL BASE DA API
-// ==============================================
+const API_BASE = "https://dog.ceo/api";
 
-// Endereço principal da API
-const API_BASE = "https://dog.ceo/api"
+//===========================
+// 3- FUNÇÃO QUE CHAMA A API
+//===========================
 
-
-// ==============================================
-// 3 - FUNÇÃO QUE CHAMA A API
-// ==============================================
-
-// função assíncrona que faz requisição do HTTP 
 async function fetchFromApi(endpoint){
-    // adiciona classe  de loading (mostra "Carregando...")
-    dogArea.classList.add("loading")
 
-    try{
-        // montar a URL completa da requisição
+    dogArea.classList.add("loading");
+
+    try {
+
         const url = `${API_BASE}${endpoint}`;
+        console.log("Requisição:", url);
 
-        // mostrar no console a URL chamada
-        console.log("Requisição: ", url);
-
-        // faz requisição HTTP para API
         const response = await fetch(url);
 
-        // Converter para JSON
+        if(!response.ok){
+            throw new Error("Erro na requisição");
+        }
+
         const data = await response.json();
+        console.log("Resposta:", data);
 
-        // Mostrar a resposta no console
-        console.log("Resposta: ", data);
+        if (data.status === "success") {
 
-        // Verificar se a API retornou com sucesso
-        if(data.status === "success"){
-            // URL da imagem retornada pela API
-            const imageURL = data.message;
-            // colocar a imagem no element <img>
-            dogImage.src = imageURL;
-            // extrai a raça da URL
-            const breed = imageURL.split("/")[4];
-            // substituir "-" por espaço
-            const formattedBreed = breed.replace(/-/g, " ");
+            const imageUrl = data.message;
+            dogImage.src = imageUrl;
 
-            // deixa a letra maiúscula
+            const breed = imageUrl.split("/")[4];
+            const formattedBreed = breed.replace(/-/g," ");
+
             const finalBreed =
-            formattedBreed.charAt(0).toUpperCase() + 
-            formattedBreed.slice(1);
+                formattedBreed.charAt(0).toUpperCase() +
+                formattedBreed.slice(1);
 
-            // mostra o nome da raça na tela
             breedName.textContent = finalBreed;
+        }
 
-        } 
+    } catch (error){
 
-    }catch (erro){
-        // mostrar erro no console 
-        console.log("Erro:", error)
+        console.error("Erro:", error);
 
-        // mensagem de erro para o usuário
-        breedName.textContent = "Erro ao carregar"
-
-        // remove imagem
+        breedName.textContent = "Erro ao carregar";
         dogImage.src = "";
+
     } finally {
-        // remove estado de loading
+
         dogArea.classList.remove("loading");
+
     }
 }
 
-//fetchFromApi("/breeds/image/random")
-//=======================================================
-//4- FUNÇÃO DE AÇÃO
-//========================================================
+//===========================
+// 4- FUNÇÕES DE AÇÃO
+//===========================
 
-
-//BUSCAR UM CACHORRO ALEATORIO
 function getRandomDog(){
-    fetchFromApi('/breeds/image/random')
+    fetchFromApi("/breeds/image/random");
 }
 
-
-// busca cachorro por raça
 function getBreedDog(){
-    // pegar o valor digitado
+
     let breed = breedInput.value.toLowerCase().trim();
 
-    // verificar se o campus tá vazio
-    if(!breed){
+    if (!breed){
         alert("Digite uma raça!");
-        return
+        return;
     }
 
-    // chamar a API
-    fetchFromApi(`/breed/&{breed}/image/random`);
+    fetchFromApi(`/breed/${breed}/images/random`);
 }
 
+//===========================
+// 5- EVENTOS
+//===========================
 
-// =================================
-// 5 - EVENTOS
-// =================================
-
-// clique no botão "aleatório"
 randomBtn.addEventListener("click", getRandomDog);
 
-// clique no botão "buscar"
 searchBtn.addEventListener("click", getBreedDog);
 
-// clique na imagem gera novo cachorro
-dogImage.addEventListener("click", getRandomDog)
+dogImage.addEventListener("click", getRandomDog);
 
-//
 breedInput.addEventListener("keypress", function(event){
-    // se a tecla pressionada for ENTER
+
     if(event.key === "Enter"){
         getBreedDog();
     }
 });
 
-
 getRandomDog();
-
-
